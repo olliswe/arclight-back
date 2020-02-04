897f7d44053f7f8c14419d25219699de5780490b
olliswe <oliveriyer@gmail.com>
2020-02-04 14:46:33 +0000

    video upload proof of concept completed

--- api/models.py
+++ api/models.py
@@ -8,4 +8,6 @@ def upload_location_video(instance, filename):
 
 
 class VideoUpload(models.Model):
+    name = models.CharField(verbose_name="Patient Name", max_length=1000)
+    dob = models.DateField(verbose_name="Date of Birth")
     file = models.FileField(upload_to=upload_location_video)
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework import views
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from api.models import VideoUpload
+from django.utils.dateparse import parse_datetime
 
 
 class FileUploadView(views.APIView):
@@ -12,6 +13,9 @@ class FileUploadView(views.APIView):
         print(request.POST)
         print(request.FILES)
         file_obj = request.FILES["file"]
-        VideoUpload.objects.create(file=file_obj)
+        name = request.POST["name"]
+        dob = request.POST["dob"]
+        dob_parsed = parse_datetime(dob)
+        VideoUpload.objects.create(file=file_obj, name=name, dob=dob_parsed.date())
         # do some stuff with uploaded file
         return Response(status=204)
