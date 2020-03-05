5e933af26cc4277d2efd0dd3c6fe71ea7935819a
olliswe <oliveriyer@gmail.com>
2020-03-05 10:22:00 +0000

    reset DB

--- api/migrations/0008_auto_20200302_0739.py
+++ api/migrations/0008_auto_20200302_0739.py
@@ -5,23 +5,20 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('api', '0007_auto_20200214_0749'),
-    ]
+    dependencies = [("api", "0007_auto_20200214_0749")]
 
     operations = [
-        migrations.RemoveField(
-            model_name='videoupload',
-            name='patient',
+        migrations.RemoveField(model_name="videoupload", name="patient"),
+        migrations.AddField(
+            model_name="videoupload",
+            name="dob",
+            field=models.DateField(blank=True, null=True, verbose_name="Date of Birth"),
         ),
         migrations.AddField(
-            model_name='videoupload',
-            name='dob',
-            field=models.DateField(blank=True, null=True, verbose_name='Date of Birth'),
-        ),
-        migrations.AddField(
-            model_name='videoupload',
-            name='name',
-            field=models.CharField(blank=True, max_length=1000, null=True, verbose_name='Patient Name'),
+            model_name="videoupload",
+            name="name",
+            field=models.CharField(
+                blank=True, max_length=1000, null=True, verbose_name="Patient Name"
+            ),
         ),
     ]
